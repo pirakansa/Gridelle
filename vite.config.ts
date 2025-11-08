04d7d5c37ae0680f82124a6ce36cecaabf1307db
pirakansa <pirakansa@users.noreply.github.com>
2025-11-08 15:13:50 +0900

    feat(pwa): add PWA support with icons and manifest configuration
    - Added apple-touch-icon.png, favicon.png, icon-192x192.png, and icon-512x512.png to public/images/
    - Created robots.txt for search engine directives
    - Configured VitePWA plugin in vite.config.ts with manifest details and asset inclusion

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { visualizer } from 'rollup-plugin-visualizer'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
@@ -12,6 +13,43 @@ export default defineConfig({
   plugins: [
     react(),
     visualizer({ filename: 'bundle-analysis.html' }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        '/images/favicon.png',
+        '/images/apple-touch-icon.png',
+        '/assets/logo-*.jpeg'
+      ],
+      manifest: {
+        name: "Gridelle",
+        short_name: "Gridelle",
+        description: "Gridelle's sandbox application.",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        lang: "ja",
+        background_color: "#fafbf5",
+        icons: [
+          {
+            src: "/images/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/images/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      },
+      devOptions: {
+        enabled: false,
+      },
+      workbox: {
+        // ignoreURLParametersMatching: [/.*/],
+        navigateFallbackDenylist: [/\/login\.html/],
+      }
+    })
   ],
   css: {
     postcss: {
